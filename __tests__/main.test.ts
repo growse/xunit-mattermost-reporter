a4ff6d91ceff32d2f5552b830536950fab7762f5
Andrew Rowson <github@growse.com>
2020-04-09 17:31:21 +0100

    Added github context

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,19 +1,23 @@
-import {collectXUnitData} from "../src/xunit"
+import {collectXUnitData} from '../src/main'
 
 test('non-existent path', async () => {
-    const path = ""
-    await expect(collectXUnitData(path)).rejects.toThrow("ENOENT: no such file or directory, stat")
+  const path = ''
+  await expect(collectXUnitData(path)).rejects.toThrow(
+    'ENOENT: no such file or directory, stat'
+  )
 })
 
 test('single test fixture', async () => {
-    const path = __dirname + "/fixtures/xunit-fixture.xml"
-    const junitResults = await collectXUnitData(path)
-    expect(junitResults.testsuites.length).toEqual(1)
-    expect(junitResults.testsuites[0].name).toEqual("org.owntracks.android.data.repos.MemoryContactsRepoTest")
+  const path = __dirname + '/fixtures/xunit-fixture.xml'
+  const junitResults = await collectXUnitData(path)
+  expect(junitResults.testsuites.length).toEqual(1)
+  expect(junitResults.testsuites[0].name).toEqual(
+    'org.owntracks.android.data.repos.MemoryContactsRepoTest'
+  )
 })
 
 test('multiple test fixture', async () => {
-    const path = __dirname + "/fixtures/multiple/"
-    const junitResults = await collectXUnitData(path)
-    expect(junitResults.testsuites.length).toEqual(2)
+  const path = __dirname + '/fixtures/multiple/'
+  const junitResults = await collectXUnitData(path)
+  expect(junitResults.testsuites.length).toEqual(2)
 })
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,119 @@
-import * as core from '@actions/core'
-import {collectXUnitData} from './xunit'
+import core from '@actions/core'
+import {context} from '@actions/github'
 
+import bent from 'bent'
 
-const bent = require('bent')
+import Parser from 'junitxml-to-javascript'
+import * as path from 'path'
+import * as fs from 'fs'
+
+export interface JunitSuite {
+  name: String
+}
+
+export interface JunitResults {
+  testsuites: JunitSuite[]
+}
+
+export interface MattermostAttachmentField {
+  short: boolean
+  title: string
+  value: string
+}
+
+export interface MattermostAttachment {
+  fallback: string
+  color: string
+  pretext?: string
+  text: string
+  author_name: string
+  author_icon?: URL
+  author_link?: URL
+  title: string
+  title_link?: URL
+  fields: MattermostAttachmentField[]
+  image_url?: URL
+}
+
+export async function collectXUnitData(
+  xunitPath: fs.PathLike
+): Promise<JunitResults> {
+  const pathStat = fs.statSync(xunitPath)
+  if (pathStat.isFile()) {
+    return new Parser()
+      .parseXMLFile(xunitPath)
+      .then(report => report as JunitResults)
+  }
+  if (pathStat.isDirectory()) {
+    return fs
+      .readdirSync(xunitPath)
+      .filter(p => fs.statSync(path.join(xunitPath.toString(), p)).isFile())
+      .map(
+        async p =>
+          new Parser().parseXMLFile(
+            path.join(xunitPath.toString(), p)
+          ) as Promise<JunitResults>
+      )
+      .reduce(async (promiseChain, currentTask) => {
+        return promiseChain.then(async chainResults => {
+          return currentTask.then(currentResult => {
+            if (core) {
+              currentResult.testsuites
+                .map(suite => `Adding report for ${suite}`)
+                .forEach(msg => core.info(msg))
+            }
+            const mergedResults: JunitResults = {
+              testsuites: chainResults.testsuites.concat(
+                currentResult.testsuites
+              )
+            }
+            return mergedResults
+          })
+        })
+      }, Promise.resolve({testsuites: []}))
+  }
+  throw new Error("Given path isn't a directory or file")
+}
+
+export function renderReportToMarkdown(
+  report: JunitResults
+): MattermostAttachment {
+  context.workflow
+  return {
+    author_name: 'Xunit Mattermost reporter',
+    color: '#00aa00',
+    fallback: 'Fallback text',
+    fields: [],
+    text: `Xunit report for ${report.testsuites.length} test suites on ${context.workflow}`,
+    title: 'Test Title'
+  }
+}
 
 async function run(): Promise<void> {
-    try {
-        const xunitPath: string = core.getInput('xUnitTestPath')
-        const mattermostWebhookUrl: string = core.getInput('mattermostWebhookUrl')
-        core.debug(`Pulling xunit results from  ${xunitPath}`)
-        const mmPost = bent('POST', 'json')
-        collectXUnitData(xunitPath)
-            .then(report => {
-                    let mmBody = {"username": "Github actions runner", "text": "test"}
-                    return mmPost(mattermostWebhookUrl, mmBody)
-                }
-            ).then(result => {
-            console.log(result)
-            core.setOutput('Mattermost response', `Success: ${result}`)
-        })
-
-    } catch (error) {
-        core.setFailed(error.message)
+  try {
+    const xunitPath: string = core.getInput('xUnitTestPath')
+    const mattermostWebhookUrl: string = core.getInput('mattermostWebhookUrl')
+    core.debug(`Pulling xunit results from  ${xunitPath}`)
+    const mmPost = bent('POST', 'json')
+    collectXUnitData(xunitPath)
+      .then(async report => {
+        const mmBody = {
+          username: 'Github actions runner',
+          text: 'test',
+          props: {attachments: [renderReportToMarkdown(report)]}
+        }
+        return mmPost(mattermostWebhookUrl, mmBody)
+      })
+      .then(result => {
+        if (core) {
+          core.setOutput('Mattermost response', `Success: ${result}`)
+        }
+      })
+  } catch (error) {
+    if (core) {
+      core.setFailed(error.message)
     }
+  }
 }
 
 run()
