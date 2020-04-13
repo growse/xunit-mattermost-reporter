85dcf8cbb442cfc5f1a7543c5d4b9dda7b65e6e5
Andrew Rowson <github@growse.com>
2020-04-13 17:03:30 +0100

    Simplified tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,5 @@
 import {collectXUnitData} from '../src/xunit'
-import {
-  generateTableMarkdownFromReport,
-  summarizeReport
-} from '../src/mattermost'
+import {renderReportToMattermostAttachment} from '../src/mattermost'
 
 jest.mock('@actions/core', () => ({
   getInput: jest.fn(),
@@ -12,32 +9,45 @@ jest.mock('@actions/core', () => ({
   startGroup: jest.fn(),
   endGroup: jest.fn()
 }))
-
+jest.mock('@actions/github', () => ({
+  context: {
+    actor: 'test-actor',
+    ref: 'test-ref',
+    workflow: 'test-workflow',
+    repo: {
+      owner: 'test-owner',
+      repo: 'test-repo'
+    },
+    payload: {
+      sender: {
+        html_url: 'https://example.com/sender',
+        avatar_url: 'https://example.com/sender.png'
+      },
+      repository: {html_url: 'https://example.com/test-owner/test-repo'},
+      pull_request: {number: 1}
+    }
+  }
+}))
 describe('Test mattermost renderings', () => {
-  test('test summary', async () => {
-    const path = __dirname + '/fixtures/multiple/'
-    const junitResults = await collectXUnitData(path)
-    const summary = summarizeReport(junitResults)
-    expect(summary.tests).toEqual(23)
-    expect(summary.succeeded).toEqual(22)
-    expect(summary.skipped).toEqual(0)
-    expect(summary.errors).toEqual(1)
-    expect(summary.duration).toEqual(53.62)
-  })
-  test('Report renders table properly', async () => {
+  test('Single report renders attachment properly', async () => {
     const path = __dirname + '/fixtures/xunit-fixture.xml'
     const junitResults = await collectXUnitData(path)
-    const table = generateTableMarkdownFromReport(junitResults)
-    expect(table).toEqual(
-      '| Test suite | Results |\n|:---|:---|\n| org.owntracks.android.data.repos.MemoryContactsRepoTest (26.802s) | :tada: 5 tests, 5 passed, 0 skipped |\n| **Total (26.802s)** | **5 tests, 5 passed, 0 failed, 0 skipped** |'
+    const message = renderReportToMattermostAttachment(junitResults)
+    expect(message.color).toEqual('#00aa00')
+    expect(message.title).toEqual('Test run success')
+    expect(message.text).toEqual(
+      '![test-actor avatar](https://example.com/sender.png&size=18) [test-actor](https://example.com/sender) ran some tests ran on [#1 undefined](https://example.com) at [test-owner/test-repo](https://example.com/test-owner/test-repo) as part of the [test-workflow](https://example.com/test-owner/test-repo/actions?query=workflow%3Atest-workflow) workflow.\n| Test suite | Results |\n|:---|:---|\n| org.owntracks.android.data.repos.MemoryContactsRepoTest (26.802s) | :tada: 5 tests, 5 passed, 0 skipped |\n| **Total (26.802s)** | **5 tests, 5 passed, 0 failed, 0 skipped** |'
     )
   })
-  test('Report renders table properly when there are failures', async () => {
-    const path = __dirname + '/fixtures/xunit-failure-fixture.xml'
+
+  test('Multi report with failures renders attachment properly', async () => {
+    const path = __dirname + '/fixtures/multiple/'
     const junitResults = await collectXUnitData(path)
-    const table = generateTableMarkdownFromReport(junitResults)
-    expect(table).toEqual(
-      '| Test suite | Results |\n|:---|:---|\n| org.owntracks.android.services.MessageProcessorEndpointHttpTest (2.117s) | :rotating_light: 7 tests, 1 failed |\n| **Total (2.117s)** | **7 tests, 6 passed, 1 failed, 0 skipped** |'
+    const message = renderReportToMattermostAttachment(junitResults)
+    expect(message.color).toEqual('#aa0000')
+    expect(message.title).toEqual('Test run failure')
+    expect(message.text).toEqual(
+      '![test-actor avatar](https://example.com/sender.png&size=18) [test-actor](https://example.com/sender) ran some tests ran on [#1 undefined](https://example.com) at [test-owner/test-repo](https://example.com/test-owner/test-repo) as part of the [test-workflow](https://example.com/test-owner/test-repo/actions?query=workflow%3Atest-workflow) workflow.\n| Test suite | Results |\n|:---|:---|\n| org.owntracks.android.data.repos.MemoryContactsRepoTest (26.802s) | :tada: 5 tests, 5 passed, 0 skipped |\n| org.owntracks.android.support.ParserTest (24.701s) | :tada: 11 tests, 11 passed, 0 skipped |\n| org.owntracks.android.services.MessageProcessorEndpointHttpTest (2.117s) | :rotating_light: 7 tests, 1 failed |\n| **Total (53.62s)** | **23 tests, 22 passed, 1 failed, 0 skipped** |'
     )
   })
 })
