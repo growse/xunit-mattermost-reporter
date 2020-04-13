ab90caa37241ade41c6ca78f95b31a5764dabaa1
Andrew Rowson <github@growse.com>
2020-04-13 16:19:16 +0100

    Table output rendering

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,5 +1,5 @@
 import {collectXUnitData} from '../src/xunit'
-import {postReportToMatterMost} from '../src/mattermost'
+import {generateTableMarkdownFromReport} from '../src/mattermost'
 
 jest.mock('@actions/core', () => ({
   getInput: jest.fn(),
@@ -9,6 +9,17 @@ jest.mock('@actions/core', () => ({
   startGroup: jest.fn(),
   endGroup: jest.fn()
 }))
+
+describe('Test mattermost renderings', () => {
+  test('Report renders table properly', async () => {
+    const path = __dirname + '/fixtures/xunit-fixture.xml'
+    const junitResults = await collectXUnitData(path)
+    const table = generateTableMarkdownFromReport(junitResults)
+    expect(table).toEqual(
+      '| Test suite | Results |\n|:---|:---|\n| org.owntracks.android.data.repos.MemoryContactsRepoTest (26.802s) | :tada: 5 tests, 5 passed, 0 skipped |\n| **Total (26.802s** | **5 tests, 5 passed, 0 failed, 0 skipped** |'
+    )
+  })
+})
 
 describe('Test XUnit report merging', () => {
   test('non-existent path', async () => {
